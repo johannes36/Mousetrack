bdf30017d742c29f6dae6df30a75ae7a11b9a695
johannes36 <johannessteinmetz97@gmail.com>
2022-09-09 18:38:40 +0200

    Simple Heatmap erstellt
    simple function to plot heatmap created. 
    still needs work.
    function to save Data created

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ import seaborn
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 #to install requirements use:
 # pip install -r requirements.txt
 
-move_x = []   #Liste der Mauspositionen
-move_y = []
-click_x = []  #Lister der Klickpositionen
-click_y = []
-time_move = [] #Liste der Zeitpunkte der Bewegungen
-time_click = [] #Liste der Zeitpunkte der Klicks
-
+move_x   = []   #Liste der Mauspositionen
+move_y   = []
+click_x  = []  #Lister der Klickpositionen
+click_y  = []
+time_move   = [] #Liste der Zeitpunkte der Bewegungen
+time_click  = [] #Liste der Zeitpunkte der Klicks
 
 # The callback to call when mouse move events occur
 def on_move(x, y):
@@ -51,106 +51,79 @@ def StartPositionTrack():
     starttime = time.time()
 
     # nächste seite anzeigen
-
-
 def PausePositionTrack():
     # close file when tracking is paused
     pass
-
 
 def ContinuePositionTrack():
     # file reopen open('a')''
     pass
 
-
 def StopPositionTrack():
     # mouse Tracking stoppen
     listener.stop()
 
+    Save2D_Data_with_Time(move_x, move_y, time_move, filename= "move.csv")
+    Save2D_Data_with_Time(click_x, click_y, time_click, filename= "click.csv")
 
-    #Daten in Heatmaps darstellen
-    #Häufigkeit des Auftreffen eines Positionspaars muss ermittelt werden
-    #np.array mit Index als Position (x, y) und Häufgkeit des Auftreffen dieser Position
-    
-    # print("move_x:" + str(move_x))
-    # print(type(move_x))
-    # print(np.shape(move_x))
+    heatmove = Calculate_Heatmap(move_x, move_y, name="Heatmap Movement")
+    # heatclick = Calculate_Heatmap(click_x, click_y, name="Heatmap Clicks")
 
-    #sortieren der movement listen
-    #evtl. datenspeicerung to csv vorschieben um Variablen zu sparen
-    move_x_sort = np.sort(move_x)
-    move_y_sort = np.sort(move_y)
-
-    # print("move_x_sort nach Initialisierung:" + str(move_x_sort))
-    # print(type(move_x_sort))
-    # print(np.shape(move_x_sort))
-    # print("Länge von move_x_sort:" + str(len(move_x_sort)))
+    #[0,0] leer, Lösung finden! Daten anders Speichern z.b.
+    pd.DataFrame(heatmove).to_csv('heatmap_move.csv')
+    # pd.DataFrame(heatclick).to_csv('heatmap_move.csv')
 
 
-    # print("maximmum Pixel in x Richtung: " + str(max(move_x_sort)))
-    # print("maximmum Pixel in x Richtung: " + str(max(move_x)))
-    
+    #Save_Heatmap(heatmove, filename="heatmap_move.csv")
+    #Save_Heatmap(heatclick, filename="heatmap_click.csv")
 
-    heat_move = np.zeros(shape=(max(move_x_sort), max(move_y_sort)))
-    
-    print("heat_move nach Initialisierung:" + str(heat_move))
-    print(type(heat_move))
-    print(np.shape(heat_move))
+def Calculate_Heatmap(x_Data, y_Data, name):
 
-    for i in range(len(move_x_sort)): #Pixel in x-Richtung/ schönere Methode überlegen um Pixel des Bildschirm zu bekommen        
-        for j in range(len(move_y_sort)):
-            #Bedingung für letzten Schhleifendurchlauf (move[x + 1] existiert nicht!)
-            # if i == len(move_x_sort) - 1 and j == len(move_y_sort) - 1:
-            if i == len(move_x_sort) - 1:# and j == len(move_y_sort) - 1:
-   
-                print("letzter Schleifendurchlauf bei: i= " + str(i) + "j=" + str(j))
-                break
-            
-            # elif i == len(move_x_sort) - 1 or j == len(move_y_sort) - 1:      
-            elif j == len(move_y_sort) - 1:      
+    heatmap = np.zeros(shape=(max(x_Data),max(y_Data)))
+    print(type(heatmap))
+    print(np.shape(heatmap))
 
-                #print("out of range Schleifendurchlauf bei: i= " + str(i) + "j=" + str(j))
-                continue
-            
-            else:
-                #wenn vorherige Position gleich aktueller Position
-                
-                pixel_x = move_x_sort[i] - 1
-                pixel_y = move_y_sort[j] - 1
-
-                #print("aktueller Schleifendurchlauf bei: i= " + str(i) + "j=" + str(j))
-                #print("aktueller x-Pixel:", pixel_x)
-                #print("aktueller y-Pixel:", pixel_y)
-
-                if move_x_sort[i] == move_x_sort[i+1] and move_y_sort[j] == move_y_sort[j+1]:
-                    heat_move[pixel_x, pixel_y] = heat_move[pixel_x, pixel_y] + 1
+    for x in x_Data:
+        for y in y_Data:
+            heatmap[x-1, y-1] = heatmap[x-1, y-1] + 1
 
 
-    print(np.shape(heat_move))
-    #Heatmap plotten
-    # seaborn.heatmap(heat_move)
+    plt.imshow(heatmap, cmap='gray')
+    plt.title(name)
+    plt.show()
+
+    return heatmap
     
 
-    
-    #Bewegungsdaten und Klicks mit Zeitpunkten in File speichern
-    with open("move.csv", "w") as file1:
-        for pos in move_x, move_y, time_move:
-            file1.writelines(str(pos) + '\n')
-    
-    with open("click.csv", "w") as file2:
-        for pos in click_x, click_y, time_click:
-            file2.writelines(str(pos) + '\n')
+def Save2D_Data_with_Time(data_x, data_y, time, filename):
+    #function to write data in csv file with timepoints
+    #data has 3 inputs, x, y and time 
+    #daten und Klicks mit Zeitpunkten in File speichern
 
-    # with open("heatmap_move.csv", "w") as file3:
-    #     for pos in heat_move:
-    #         file3.writelines(str(pos) + '\n')
-    pd.DataFrame(heat_move).to_csv("heatmap_move.csv")
+        # if os.path.exists("demo.txt"):
+    #     #kreiere eine neue Datei, mit anderer Endung
+    #     os.remove("demo.txt")
+    #     file = open("demo.txt", "w")
+    # else:
+    #     file = open("demo.txt", "x")
+
+    with open(filename, "w") as file:
+        for pos in data_x, data_y, time:
+            file.writelines(str(pos) + '\n')
+
+def Save_Heatmap(data, filename):
+    with open(filename, "w") as file:
+        for x, y in data:
+            file.writelines(str(x) + '\n')
+            file.writelines(str(y) + '\n')
 
 
+# def Save    
 
 
 app = tk.Tk()
 app.title("Mousetrack")
+
 
 wholePage = tk.Frame(app).grid()
 
@@ -158,7 +131,7 @@ label1 = tk.Label(wholePage, text="Input 1:").grid(row=0, column=0, rowspan=2, c
 label2 = tk.Label(wholePage, text="Check 1:").grid(row=2, column=0, rowspan=2, columnspan=4)
 
 # start only once callable, for new start, new thread has to be started
-button_Start = ttk.Button(wholePage, text="Starte Anwendung", command=lambda: StartPositionTrack()).grid(row=4, column=0)  # hier später timer starten
+button_Start = ttk.Button(wholePage, text="Starte Anwendung", command=lambda: StartPositionTrack()).grid(row=4, column=0)
 button_Stopp = ttk.Button(wholePage, text="Stopp", command=lambda: StopPositionTrack()).grid(row=4, column=1)
 
 
@@ -182,10 +155,5 @@ app.mainloop()
 
        
 
-    # if os.path.exists("demo.txt"):
-    #     #kreiere eine neue Datei, mit anderer Endung
-    #     os.remove("demo.txt")
-    #     file = open("demo.txt", "w")
-    # else:
-    #     file = open("demo.txt", "x")
+
         
--- read_demo.py
+++ read_demo.py
@@ -16,11 +16,37 @@ with open('heatmap_move.csv') as csvdatei:
     for row in reader:
         heat_move.append(row)
 
-print(type(heat_move(1,1)))
-print(type(heat_move))
 print(np.shape(heat_move))
-    
-    
+print(type(heat_move))
+
+
+heat_move = np.array(heat_move, dtype='float')
+
+print(heat_move[200,0])
+print(type(heat_move[200,0]))
+print(type(heat_move))
+
+fig = plt.imshow(heat_move)
+plt.show()
+
+# print(type(heat_move))
+# print(np.shape(heat_move))
+
+# fig, ax = plt.subplots()
+# im = ax.imshow(heat_move)
+
+# ax.set_xticks(np.arange(len(heat_move[:,0])), labels="x_Werte")
+# ax.set_yticks(np.arange(len(heat_move[0,:])), labels="y_Werte")
+
+# for i in range(len(heat_move[:,0])):
+#     for j in range(len(heat_move[0,:])):
+#         text = ax.text(j, i, heat_move[i, j],
+#                        ha="center", va="center", color="w")
+
+
+# fig.tight_layout()
+# plt.show()
+
 # for row in csv_reader_object:
     #print(row)
     
