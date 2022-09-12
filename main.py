Corrected Heatmap calculation
Calculation of Heatmap works fine
plot still needs work

main.py
@@ -21,10 +21,11 @@ click_y  = []
 time_move   = [] #Liste der Zeitpunkte der Bewegungen
 time_click  = [] #Liste der Zeitpunkte der Klicks
 
+print('START')
 # The callback to call when mouse move events occur
 def on_move(x, y):
 
-    print('Maus bewegt zu {0}'.format((x, y)))
+    #print('Maus bewegt zu {0}'.format((x, y)))
     # Daten in Liste abspeichern
     move_x.append(x)
     move_y.append(y)
@@ -73,24 +74,25 @@ def StopPositionTrack():
     pd.DataFrame(heatmove).to_csv('heatmap_move.csv')
     # pd.DataFrame(heatclick).to_csv('heatmap_move.csv')
 
+    CalculateVelocity()
+
 def CalculateVelocity():
-    pass
+    print('Velocity calculated')
 
 def CalculateAcceleration():
     pass
 
 def CalculateHeatmap(x_Data, y_Data, name):
 
-    heatmap = np.zeros(shape=(max(x_Data),max(y_Data)))
+    heatmap = np.zeros(shape=(max(y_Data), max(x_Data)))
     print(type(heatmap))
     print(np.shape(heatmap))
 
-    for x in x_Data:
-        for y in y_Data:
-            heatmap[x-1, y-1] = heatmap[x-1, y-1] + 1
+    for i in range(len(x_Data)):
+        heatmap[y_Data[i] - 1, x_Data[i] - 1] = heatmap[y_Data[i] - 1, x_Data[i] - 1] + 1
 
-
-    plt.imshow(heatmap, cmap='gray')
+    print(np.max(np.max(heatmap)))
+    plt.imshow(heatmap) #, cmap='gray')
     plt.title(name)
     plt.show()
 